0431d7122ddaf9c7a79c40c4228734ef76fe0796
Conrado Werlang <conradowerlang@Mac-mini-de-Conrado.local>
2025-08-12 17:14:05 -0300

    feat: add the NewsFeedView to the MainTabView first tab

--- DesafioFrameworkNative/App/MainTabView.swift
+++ DesafioFrameworkNative/App/MainTabView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 struct MainTabView: View {
     var body: some View {
         TabView {
-            Text("Notícias")
+            NewsFeedView()
                 .tabItem {
                     Label("Notícias", systemImage: "newspaper")
                 }
